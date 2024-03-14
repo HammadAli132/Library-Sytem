8739958ac6045ce98997441662a045a668d623ab
HammadAli132 <alihammad132@outlook.com>
2024-03-14 23:34:34 +0500

    Implemented Logout feature and fixed sliding login form

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 const year = document.getElementById("year");
 year.innerText = new Date().getFullYear();
 const signIn = document.getElementById("sign-in");
-const signUp = document.getElementById("sign-up");
+const logOut = document.getElementById("log-out");
 const signInPage = document.querySelector(".sign-in-form");
 const form = document.getElementById("login-form");
 const userName = document.getElementById("user-name");
 const userEmail = document.getElementById("user-email");
 const userPhNo = document.getElementById("user-phNo");
+const cancelBtn = document.getElementById("cancel");
 
 signIn.addEventListener('click', () => {
     signInPage.classList.toggle("display-form");
 });
-signUp.addEventListener('click', () => {
-    signInPage.classList.toggle("display-form");
+logOut.addEventListener('click', () => {
+    userName.innerText = "-";
+    userEmail.innerText = "-";
+    userPhNo.innerText = "-";
 });
 
 form.addEventListener('submit', (e) => {
@@ -24,9 +27,13 @@ form.addEventListener('submit', (e) => {
     userName.innerText = `${fName} ${lName}`;
     userEmail.innerText = email;
     userPhNo.innerText = phNumber;
-    console.log(fName);
-    console.log(lName);
-    console.log(email);
-    console.log(phNumber);
+    signInPage.classList.remove("display-form");
+});
+
+cancelBtn.addEventListener("click", () => {
+    document.getElementById("f-name").value = "";
+    document.getElementById("l-name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("phNo").value = "";
     signInPage.classList.remove("display-form");
 });
