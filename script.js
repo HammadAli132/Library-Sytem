f2b0f833a0b27dabf484229a842b939490888111
HammadAli132 <alihammad132@outlook.com>
2024-03-15 18:10:40 +0500

    Implemented grid layout, book-card, and add-book-card. Also, made the add-book form. Removed some bugs from sign-in form and add-book form

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const form = document.getElementById("login-form");
 const userName = document.getElementById("user-name");
 const userEmail = document.getElementById("user-email");
 const userPhNo = document.getElementById("user-phNo");
-const cancelBtn = document.getElementById("cancel");
+const profileCancelBtn = document.querySelector(".signin-cancel");
+const bookCancelBtn = document.querySelector(".book-cancel");
 
 signIn.addEventListener('click', () => {
     signInPage.classList.toggle("display-form");
@@ -28,12 +29,30 @@ form.addEventListener('submit', (e) => {
     userEmail.innerText = email;
     userPhNo.innerText = phNumber;
     signInPage.classList.remove("display-form");
+    addBookPage.classList.remove("display-form");
 });
 
-cancelBtn.addEventListener("click", () => {
+profileCancelBtn.addEventListener("click", () => {
     document.getElementById("f-name").value = "";
     document.getElementById("l-name").value = "";
     document.getElementById("email").value = "";
     document.getElementById("phNo").value = "";
     signInPage.classList.remove("display-form");
+});
+
+const addBookBtn = document.getElementById("add-book-card");
+const addBookPage = document.querySelector(".book-form-bg");
+
+addBookBtn.addEventListener('click', () => {
+    addBookPage.classList.toggle("display-form");
+});
+
+bookCancelBtn.addEventListener('click', () => {
+    document.getElementById("b-name").value = "";
+    document.getElementById("b-author").value = "";
+    document.getElementById("b-pages").value = "";
+    document.getElementById("read").checked = false;
+    document.getElementById("pending").checked = false;
+    document.getElementById("unread").checked = false;
+    addBookPage.classList.remove("display-form");
 });
